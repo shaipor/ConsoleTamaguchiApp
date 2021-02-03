1fc794faf02fd6e122de6e15afad52d3b26e06c2
shai porat <shai.porat.1@gmail.com>
2021-02-03 14:30:55 +0200

    Added DTO calsses

---
+++ DataTransferObjects/ActionsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTamaguchiApp.DataTransferObjects
+{
+    class ActionsDTO
+    {
+        public int actionId { get; set; }
+        public string actionName { get; set; }
+        public int actionTypeId { get; set; }
+        public int actionEffection { get; set; }
+
+        public ActionsDTO() { }
+    }
+}
