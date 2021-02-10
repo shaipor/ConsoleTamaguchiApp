92945f55e3d1a6e357bc9573a8c08671f3001cc7
danielle oz <danielle.oz.do@gmail.com>
2021-02-10 15:20:22 +0200

    eeeeeeer

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -14,6 +14,7 @@ namespace Tamagotchi.UI
             //Build items in main menu!
             AddItem("Show Player", new PlayerScreen());
             AddItem("feed animal", new FeedingScreen());
+            AddItem("play with your animal", new PlayingScreen());
             //AddItem("watch actions history", new ActionsHistoryScreen());
             //AddItem("show animal", new PetInformationScreen());
 
